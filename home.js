be00786405d0ace462a95bbc900471529ce15eb9
LovePathak4 <lokeshpathak378@gmail.com>
2026-01-08 01:54:07 +0530

    update feature

--- home.js
+++
@@ -1,8 +0,0 @@
-var x = document.getElementById("btn");
-x.addEventListener("click", myFunction);
-
-function myFunction() {
-    const newELM= document.createElement("h1");
-    newELM.innerText = "I got all of your information sucker.. no i am going to steal all your money, BYE LIL KID";
-    document.body.appendChild(newELM);
-}
